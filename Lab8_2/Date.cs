c4cac7dff5496cf4fef29d954a8d526b787715af
Volodymyr Vyshnenko <vyshnenko2904@gmail.com>
2020-11-26 12:51:15 +0200

    Добавьте файлы проекта.

---
+++ Lab8_2/Date.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab8_2
+{
+    class Date
+    {
+        private int date;
+
+        public Date(int date)
+        {
+            this.date = date;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                int temp = date + index;
+                if (temp > 31)
+                {
+                    while (temp > 31)
+                    {
+                        temp -= 31;
+                    }
+                    return temp;
+                }
+                else if (temp < 0)
+                {
+                    while (temp < -31)
+                    {
+                        temp += 31;
+                    }
+                    return 31 + temp;
+                }
+                else
+                {
+                    return temp;
+                }
+            }
+        }
+    }
+}
---
+++ Lab8_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Date date = new Date(5);
+            Console.WriteLine(date[29]);// output "3"
+            Console.ReadLine();
+        }
+    }
+}
